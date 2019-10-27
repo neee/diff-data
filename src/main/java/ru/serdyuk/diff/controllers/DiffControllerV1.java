0eba68180ef6c7ddd64460d533cbf58faf103068
Sergey Serdyuk <serdyuksa@yandex-team.ru>
2019-10-27 22:58:46 +0300

    code style

--- src/main/java/ru/serdyuk/diff/controllers/DiffControllerV1.java
+++ src/main/java/ru/serdyuk/diff/controllers/DiffControllerV1.java
@@ -2,7 +2,6 @@ package ru.serdyuk.diff.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
