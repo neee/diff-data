1e6618338385ff85231199ac9ae58a792e7852bf
Sergey Serdyuk <serdyuksa@yandex-team.ru>
2019-10-27 21:57:02 +0300

    add swagger

--- src/main/java/ru/serdyuk/diff/DiffApplication.java
+++ src/main/java/ru/serdyuk/diff/DiffApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiffApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiffApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiffApplication.class, args);
+    }
 }
--- src/main/java/ru/serdyuk/diff/DiffConfiguration.java
+++ src/main/java/ru/serdyuk/diff/DiffConfiguration.java
@@ -2,14 +2,40 @@ package ru.serdyuk.diff;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import ru.serdyuk.diff.services.DiffService;
 import ru.serdyuk.diff.services.DiffServiceStandaloneImpl;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class DiffConfiguration {
+@EnableSwagger2
+public class DiffConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
     DiffService standaloneDiffService() {
         return new DiffServiceStandaloneImpl();
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.ant("/v1/diff/*"))
+            .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
+
